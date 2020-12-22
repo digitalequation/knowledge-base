d3296bda65c3e319ac05cd4a6c088ed7da01bc7e
chiribuc <robert.chiribuc@thebug.ro>
2020-12-22 10:28:47 +0200

    [routes][api] set the right order of routes

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('kb/{category_slug?}', 'KnowledgeBaseAPIController@getIndex');
-Route::get('kb/category/{slug}', 'KnowledgeBaseAPIController@getCategory');
-Route::get('kb/article/{category_slug}/{article_slug}', 'KnowledgeBaseAPIController@getArticle');
-Route::get('kb/search/{term}', 'KnowledgeBaseAPIController@getSearch');
-
 Route::group(['middleware' => config('knowledge-base.authorization')], function () {
     Route::get('kb/update-cache', 'KnowledgeBaseAPIController@getUpdateCache');
 });
+
+Route::get('kb/article/{category_slug}/{article_slug}', 'KnowledgeBaseAPIController@getArticle');
+Route::get('kb/search/{term}', 'KnowledgeBaseAPIController@getSearch');
+Route::get('kb/category/{slug}', 'KnowledgeBaseAPIController@getCategory');
+Route::get('kb/{category_slug?}', 'KnowledgeBaseAPIController@getIndex');
