d94d8cb22c890a480e69c8cd16df75278cab83d0
chiribuc <robert.chiribuc@thebug.ro>
2020-12-22 10:04:44 +0200

    [routes][api] add middleware authorization for protected route

--- routes/api.php
+++ routes/api.php
@@ -6,4 +6,7 @@ Route::get('kb/{category_slug?}', 'KnowledgeBaseAPIController@getIndex');
 Route::get('kb/category/{slug}', 'KnowledgeBaseAPIController@getCategory');
 Route::get('kb/article/{category_slug}/{article_slug}', 'KnowledgeBaseAPIController@getArticle');
 Route::get('kb/search/{term}', 'KnowledgeBaseAPIController@getSearch');
-Route::get('kb/update-cache', 'KnowledgeBaseAPIController@getUpdateCache');
+
+Route::group(['middleware' => config('knowledge-base.authorization')], function () {
+    Route::get('kb/update-cache', 'KnowledgeBaseAPIController@getUpdateCache');
+});
